ae6011bada37e51419afed084104abb84df6960c
Jake Martin <ajanimember@gmail.com>
2017-03-03 12:55:50 +1000

    made code more presentable

--- alphabeticalSort.h
+++ alphabeticalSort.h
@@ -10,16 +10,14 @@ class alphabeticalSort{
         //this is the definition
         alphabeticalSort(std::vector<std::string>&, std::vector<std::string>&);
     private:
-        //vector that determins the position that 
-        //vector that determins the position that
-
+        
         const std::vector<char> chars;
         std::vector<std::string>& unsortedStrings;
         std::vector<std::string>& sortedStrings;
 
         void sort();
         int compare(std::string&, std::string&);
-        int findNumericValue(char);
+        int numericValue(char);
 };
 
 #endif
